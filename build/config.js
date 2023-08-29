66dfc8042eb3c012a1532a92c27369509be8b903
Dominique Cavailhez <github@c92.fr>
2023-08-29 10:47:23 +0200

    Files init from jonataswalker/ol-geocoder

---
+++ build/config.js
@@ -0,0 +1,48 @@
+import node from '@rollup/plugin-node-resolve';
+import cjs from '@rollup/plugin-commonjs'; // Convert CommonJS module into ES module
+import css from 'rollup-plugin-import-css'; // Collect css
+import json from '@rollup/plugin-json';
+import terser from '@rollup/plugin-terser'; // Rollup plugin to minify generated es bundle
+import {
+	readFileSync, // Read banner file
+} from 'fs';
+
+const banner = readFileSync('./build/banner.js', 'utf-8');
+
+export default [{
+	// Compressed library
+	input: 'build/index.js',
+	plugins: [
+		node(),
+		cjs(),
+		css({
+			output: 'ol-geocoder.css',
+		}),
+		json(),
+		terser(),
+	],
+	output: [{
+		name: 'OlGeocoder',
+		banner,
+		file: 'dist/ol-geocoder.js',
+		format: 'umd',
+		sourcemap: true,
+	}],
+}, {
+	// Debug library
+	input: 'build/index.js',
+	plugins: [
+		node(),
+		cjs(),
+		css({
+			output: 'ol-geocoder.css',
+		}),
+		json(),
+	],
+	output: [{
+		name: 'OlGeocoder',
+		banner,
+		file: 'dist/ol-geocoder-debug.js',
+		format: 'iife',
+	}],
+}];
---
+++ build/index.js
@@ -0,0 +1,5 @@
+import '../src/ol-geocoder.css';
+import Geocoder from '../src/base';
+
+export default Geocoder;
+//TODO export direct
